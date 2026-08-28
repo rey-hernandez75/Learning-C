b6cd2897b2711779cba4364f5e6fe06bc1d44f2a
Rey Hernández <perazahdezrey@gmail.com>
2026-08-28 11:48:19 -0300

    Programa 001: Bienvenido a la Programación en C

---
+++ 001-Bienvenido-a-la-Programacion-en-C/main.c
@@ -0,0 +1,16 @@
+/*
+Learning-C
+Programa 001
+Bienvenido a la Programación en C
+
+Autor: Reinel Hernández
+Compilador: GCC (C17)
+Sistema Operativo: Linux Mint 22.3 Cinnamon.
+*/
+
+#include <stdio.h>
+int main (void)
+{
+    printf("Bienvenido a la Programación en C.\n");
+    return 0;
+}
